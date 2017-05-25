6126f35b8d2bc3d71159a5841921d1f240ebfec0
Sandrine Ayral <sandrine.ayral@gmail.com>
2017-05-25 15:31:04 +0100

    still have a problem with redirection when doses are not valid

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [ :new, :create ]
+  end
+
+  resources :doses, only: [ :destroy ]
 end
